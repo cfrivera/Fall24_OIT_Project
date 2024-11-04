8ce3e056a5ea097ebea27b5670383ffdf6ef690d
Charles Rivera <85764382+cfrivera@users.noreply.github.com>
2024-11-04 15:55:30 -0800

    Add files via upload

---
+++ nlp_financial.py
@@ -0,0 +1,15 @@
+import pandas
+
+# /Users/charles/Downloads/archive (2)
+# https://www.kaggle.com/datasets/miguelaenlle/massive-stock-news-analysis-db-for-nlpbacktests
+
+# Specify the path to your CSV file
+# csv_file_path = '/Users/charles/Downloads/archive (2)/analyst_ratings_processed.csv'
+
+# # Read the CSV file into a DataFrame
+# df = pd.read_csv(csv_file_path)
+
+# # Display the first few rows of the DataFrame
+# df.head()
+
+# print("hello")
